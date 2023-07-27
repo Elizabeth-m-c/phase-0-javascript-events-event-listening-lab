76b1dea0a4dfd42a3916cd193c0f9ec8eef41348
Elizabeth-m-c <ellzabethwil937@gmail.com>
2023-07-27 15:31:42 +0300

    fisrt commit

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-function addingEventListener() {
+function addingEventListener(input) {
+    input=document.getElementById('input')
+    console.log(input)
+    input.addEventListener('keypress',addingEventListener)
+    input.innerText='Now'
+
 }
+addingEventListener() 
